95e46071efb2ffb42dd0676953b1aa6c2dec9f66
Scott Stevenson <scotifer92@googlemail.com>
2018-04-17 08:36:31 +0100

    Fully functional game and welcome page added

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require "sinatra/contrib/all" if development?
 also_reload("./models/*")
 require_relative "./models/game.rb"
 
+get "/" do
+  erb(:welcome)  
+end
+
 get "/game/:player1/:player2" do
   @result = Game.decide_match(params[:player1], params[:player2])
   erb(:result)
